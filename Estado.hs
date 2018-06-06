369da722c72b36cc13972ee03e816a0ddc40e35f
Gilney Junior <gilneyjnr@gmail.com>
2018-06-06 11:27:09 -0300

    Adicionado comentários ao Estado.hs

--- Estado.hs
+++ Estado.hs
@@ -26,13 +26,22 @@ data Valor = ValorInteiro Integer
            | ValorEstrutura [Valor]
            deriving (Show, Eq)
 
+{- Parâmetros:
+	Integer -> Id do Escopo pai
+	Estado  -> Estado atual do programa
+-}
 criarEscopo :: Integer -> Estado -> Estado
 criarEscopo idEscopoAtual (pilhaEscopo, tipos) =
             (((toInteger (length pilhaEscopo)) + 1, idEscopoAtual, []):pilhaEscopo, tipos)
 
+{- Parâmetros:
+	Integer -> Id do Escopo a ser procurado
+	Estado  -> Estado atual do programa
+-}
 getEscopoById :: Integer -> Estado -> Escopo
 getEscopoById idEscopoAtual (pilhaEscopo, _) = getEscopo' idEscopoAtual pilhaEscopo
 
+-- Função auxiliar para getEscopoById
 getEscopo' :: Integer -> [Escopo] -> Escopo
 getEscopo' _             []     = error "Pilha de escopos vazia"
 getEscopo' idEscopoAtual (escopo@(idEscopo, _, _):pilhaEscopo) =
@@ -41,12 +50,15 @@ getEscopo' idEscopoAtual (escopo@(idEscopo, _, _):pilhaEscopo) =
            else
                getEscopo' idEscopoAtual pilhaEscopo
 
+-- Retorna o escopo atual a partir do estado
 getEscopoAtual :: Estado -> Escopo
 getEscopoAtual ((escopoAtual:_), _) = escopoAtual
 
+-- Retorna o Id do escopo atual
 getIdEscopoAtual :: Estado -> Integer
 getIdEscopoAtual estado = idEscopoAtual where (idEscopoAtual, _, _) = getEscopoAtual estado
 
+-- Remove o escopo do topo da pilha
 removerEscopo :: Estado -> Estado
 removerEscopo ((escopo:pilhaEscopo), tipos) = (pilhaEscopo, tipos)
 
