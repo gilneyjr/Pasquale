b5a7759a7302ef5c0cd268790e423c543588b4f7
Joel Felipe <joelfelipe07@gmail.com>
2018-06-07 08:13:51 -0300

    Atualizando Estado.hs

--- Estado.hs
+++ Estado.hs
@@ -2,15 +2,17 @@ module Estado (
     Estado,
     Escopo,
     Simbolo,
-    Tipo,
-    Valor,
+    Tipo (..),
+    Valor (..),
+    ErroEstado (..),
     criarEscopo,
     getEscopoById,
     getEscopoAtual,
     getIdEscopoAtual,
     removerEscopo,
     addSimbolo,
-    getSimbolo
+    getSimbolo,
+    atualizarSimbolo
 ) where
 
 import Arvore
@@ -42,6 +44,8 @@ data Valor = ValorInteiro Integer
            deriving (Show, Eq)
 
 data ErroEstado = ErroNomeDuplicado String
+                | ErroBuscaSimbolo String
+                deriving (Show)
 
 estadoNulo :: Estado
 estadoNulo = ([], [])
@@ -55,23 +59,19 @@ escopoNulo = (-1, -1, [])
 -}
 criarEscopo :: Integer -> Estado -> Estado
 criarEscopo idEscopoAtual (pilhaEscopo, tipos) =
-            ((toInteger (length pilhaEscopo) + 1, idEscopoAtual, []):pilhaEscopo, tipos)
+            ((toInteger ((length pilhaEscopo) + 1), idEscopoAtual, []):pilhaEscopo, tipos)
 
 {- Parâmetros:
 	Integer -> Id do Escopo a ser procurado
 	Estado  -> Estado atual do programa
 -}
 getEscopoById :: Integer -> Estado -> Escopo
-getEscopoById idEscopoAtual estado = getEscopoByIdFromEscopos idEscopoAtual (fst estado)
+getEscopoById idEscopoAtual estado = fromJust $ getEscopoByIdFromPilha idEscopoAtual (fst estado)
 
 -- Função auxiliar para getEscopoById
-getEscopoByIdFromEscopos :: Integer -> [Escopo] -> Escopo
-getEscopoByIdFromEscopos _             []     = error "Pilha de escopos vazia"
-getEscopoByIdFromEscopos idEscopoAtual (escopo@(idEscopo, _, _):pilhaEscopo) =
-        if idEscopoAtual == idEscopo then
-                escopo
-        else
-                getEscopoByIdFromEscopos idEscopoAtual pilhaEscopo
+getEscopoByIdFromPilha :: Integer -> [Escopo] -> Maybe Escopo
+getEscopoByIdFromPilha _        []    = Nothing
+getEscopoByIdFromPilha idEscopo pilha = Just $ pilha !! fromInteger idEscopo
 
 -- Retorna o escopo atual a partir do estado
 getEscopoAtual :: Estado -> Escopo
@@ -88,28 +88,25 @@ removerEscopo ((escopo:pilhaEscopo), tipos) = (pilhaEscopo, tipos)
 -- Adicionar símbolo ao estado
 addSimbolo :: Simbolo -> Estado -> Either ErroEstado Estado
 addSimbolo simbolo estado@(escopos, tipos) = 
-        if isRight escopoAtualizado then
-                Right ((head $ rights $ [escopoAtualizado]):tail escopos, tipos)
-        else
-                Left $ head $ lefts [escopoAtualizado]
-        where escopoAtualizado = addSimboloEscopo simbolo (getEscopoAtual estado)
+        case escopo of
+                Right escopoAtualizado -> Right $ (escopoAtualizado: tail escopos, tipos)
+                Left erro -> Left erro
+        where escopo = addSimboloEscopo simbolo (getEscopoAtual estado)
 
 -- Adicionar símbolo ao escopo atual
 addSimboloEscopo :: Simbolo -> Escopo -> Either ErroEstado Escopo
-addSimboloEscopo simbolo escopo@(idEscopo, idEscopoAnterior, tabela) = 
-        if isRight tabelaAtualizada then
-                Right (idEscopo, idEscopoAnterior, head $ rights $ [tabelaAtualizada])
-        else
-                Left $ head $ lefts [tabelaAtualizada]
-        where tabelaAtualizada = addSimboloTabela simbolo tabela
+addSimboloEscopo simbolo (idEscopo, idEscopoAnterior, tabelaAtual) = 
+        case tabela of 
+                Right tabelaAtualizada -> Right (idEscopo, idEscopoAnterior, tabelaAtualizada)
+                Left erro -> Left erro
+        where tabela = addSimboloTabela simbolo tabelaAtual
 
 -- Adicionar simbolo à tabela de símbolos
 addSimboloTabela :: Simbolo -> [Simbolo] -> Either ErroEstado [Simbolo] 
-addSimboloTabela simbolo@(nome, _, _) tabela =
-        if getSimboloTabela nome tabela == Nothing then
-                Right $ simbolo:tabela
-        else
-                Left $ ErroNomeDuplicado ("Já existe uma variável com o nome:" ++ nome)
+addSimboloTabela (nome, _, _) tabela =
+        case getSimboloTabela nome tabela of
+                Just simbolo -> Right $ simbolo:tabela
+                Nothing -> Left $ ErroNomeDuplicado ("Já existe uma variável com o nome:" ++ nome)
 
 -- Busca por um símbolo na tabela de símbolos pelo nome
 getSimboloTabela :: String -> [Simbolo] -> Maybe Simbolo
@@ -121,31 +118,52 @@ getSimboloTabela nomeNovo (simbolo@(nome, _, _):tabela) =
                 getSimboloTabela nomeNovo tabela
 
 -- Busca por um símbolo no estado pelo nome
-getSimbolo :: String -> Estado -> Simbolo
+getSimbolo :: String -> Estado -> Either ErroEstado Simbolo
 getSimbolo nome estado = getSimboloPilha nome (fst estado)
 
 -- Busca por um síbolo na pilha de escopos pelo nome
-getSimboloPilha :: String -> [Escopo] -> Simbolo
+getSimboloPilha :: String -> [Escopo] -> Either ErroEstado Simbolo
 getSimboloPilha nome pilha =
-        if simbolo /= Nothing then
-                fromJust simbolo
-        else
-                error $ "Nome '" ++ nome ++ "' não encontrado na tabela de símbolos"
-        where simbolo = getSimboloPilha' nome (head pilha) pilha
+        case simbolo of
+                Just simbolo' -> Right simbolo'
+                Nothing -> Left $ ErroBuscaSimbolo $ "Nome '" ++ nome ++ "' não encontrado na tabela de símbolos"
+        where simbolo = getSimboloPilha' nome (Just (head pilha)) pilha
 
 -- Auxiliar para a busca por um símbolo na pilha
-getSimboloPilha' :: String -> Escopo -> [Escopo] -> Maybe Simbolo
-getSimboloPilha' nome escopo@(_, idEscopoAnterior, _) pilha =
-        if simbolo /= Nothing then
-                simbolo
-        else
-                getSimboloPilha' nome (getEscopoByIdFromEscopos idEscopoAnterior pilha) pilha
-        where simbolo = getSimboloEscopo nome escopo
+getSimboloPilha' :: String -> Maybe Escopo -> [Escopo] -> Maybe Simbolo
+getSimboloPilha' _ Nothing _ = Nothing
+getSimboloPilha' nome (Just (_, idEscopoAnterior, tabelaSimbolos)) pilha =
+        case simbolo of
+                Just simbolo' -> Just simbolo'
+                Nothing -> getSimboloPilha' nome (getEscopoByIdFromPilha idEscopoAnterior pilha) pilha
+        where simbolo = getSimboloTabela nome tabelaSimbolos
 
 -- Buscar por um símbolo no escopo pelo nome
 getSimboloEscopo :: String -> Escopo -> Maybe Simbolo
 getSimboloEscopo nome (_, _, tabelaSimbolos) = getSimboloTabela nome tabelaSimbolos
 
-atualizarSimbolo :: Simbolo -> Estado -> Estado
-atualizarSimbolo = undefined
---atualizarSimbolo simbolo estado = atualizarSimboloPilha simbolo (fst estado)
+atualizarSimbolo :: Simbolo -> Estado -> Either ErroEstado Estado
+atualizarSimbolo simbolo (pilhaAtual, tipos) =
+        case pilhaAtualizada of
+                Right pilhaAtualizada -> Right $ (pilhaAtualizada, tipos)
+                Left error -> Left error
+        where pilhaAtualizada = atualizarSimboloPilha simbolo pilhaAtual
+
+atualizarSimboloPilha :: Simbolo -> [Escopo] -> Either ErroEstado [Escopo]
+atualizarSimboloPilha simbolo@(nome, _, _) pilhaAtual = 
+        case pilha of
+                Just pilhaAtualizada -> Right pilhaAtualizada
+                Nothing -> Left $ ErroBuscaSimbolo $ "Nome '" ++ nome ++ "' não encontrado na tabela de símbolos"
+        where pilha = atualizarSimboloPilha' simbolo (Just (head pilhaAtual)) pilhaAtual
+
+atualizarSimboloPilha' :: Simbolo -> Maybe Escopo -> [Escopo] -> Maybe [Escopo]
+atualizarSimboloPilha' _ Nothing _ = Nothing
+atualizarSimboloPilha' simbolo (Just escopoAtual@(idEscopoAtual, idEscopoAnterior, tabelaAtual)) pilha =
+        case tabela of
+                Just tabelaAtualizada -> Just $ trocarEscopo idEscopoAtual (idEscopoAtual, idEscopoAnterior, tabelaAtualizada) pilha
+                Nothing -> atualizarSimboloPilha' simbolo (getEscopoByIdFromPilha idEscopoAnterior pilha) pilha
+        where tabela = undefined
+
+trocarEscopo :: Integer -> Escopo -> [Escopo] -> [Escopo]
+trocarEscopo i escopo pilha = inicio ++ [escopo] ++ tail fim
+        where (inicio, fim) = splitAt (fromInteger i) pilha
