440741ffae7aa1b78d8f6ae9d49169a993d6ab76
Victor <victor.agnez@hotmail.com>
2018-06-09 23:26:39 -0300

    inicio do avaliador de expressoes

--- Expressoes.hs
+++ Expressoes.hs
@@ -283,21 +283,78 @@ evaluateExpr estado (CRIAMOD expr1 op expr2) = do
         (res1,estado1) = evaluateExpr estado expr1
         (res2,estado2) = evaluateExpr estado1 expr2 
 
-evaluateExpr estado (CRIANEG op expr1) = do
+evaluateExpr estado (CRIANEG op expr) = do
     case res1 of
         ValorInteiro a -> (ValorInteiro (-a), estado1)
         ValorReal a -> (ValorReal (-a), estado1)
         otherwise -> error $ "Tipo inválido para o operador - : posição " ++ show (getposTokenOp op)
     where
-        (res1,estado1) = evaluateExpr estado expr1
+        (res1,estado1) = evaluateExpr estado expr
 
-evaluateExpr estado (CRIANOT op expr1) = do
+evaluateExpr estado (CRIANOT op expr) = do
     case res1 of
         ValorLogico a -> (ValorLogico (not a), estado1)
         otherwise -> error $ "Tipo inválido para o operador ! : posição " ++ show (getposTokenOp op)
     where
-        (res1,estado1) = evaluateExpr estado expr1
+        (res1,estado1) = evaluateExpr estado expr
 
+evaluateExpr estado (CRIACONVERSAO tipo expr) = do
+    case tipo of
+        TIPO _ "INTEIRO" -> 
+            case res1 of
+                ValorInteiro a -> (ValorInteiro a, estado1)
+                ValorReal a -> (ValorInteiro (read (show a)), estado1)
+                ValorCaractere a -> (ValorInteiro (read (show a)), estado1)
+                ValorTexto a -> (ValorInteiro (read a), estado1)
+                ValorLogico a -> (ValorInteiro (logicoToInt a), estado1)
+                otherwise -> error $ "Conversao inválida : posição " ++ show (getposTokenTipo tipo)
+        TIPO _ "REAL" -> 
+            case res1 of
+                ValorInteiro a -> (ValorReal (read (show a)), estado1)
+                ValorReal a -> (ValorReal a, estado1)
+                ValorCaractere a -> (ValorReal (read (show a)), estado1)
+                ValorTexto a -> (ValorReal (read a), estado1)
+                ValorLogico a -> (ValorReal (logicoToReal a), estado1)
+                otherwise -> error $ "Conversao inválida : posição " ++ show (getposTokenTipo tipo)
+        TIPO _ "TEXTO" ->
+            case res1 of
+                ValorInteiro a -> (ValorTexto (show a), estado1)
+                ValorReal a -> (ValorTexto (show a), estado1)
+                ValorCaractere a -> (ValorTexto (show a), estado1)
+                ValorTexto a -> (ValorTexto a, estado1)
+                ValorLogico a -> (ValorTexto (showLogico a), estado1)
+                otherwise -> error $ "Conversao inválida : posição " ++ show (getposTokenTipo tipo)
+        TIPO _ "CARACTERE" ->
+            case res1 of
+                ValorCaractere a -> (ValorCaractere a, estado1)
+                otherwise -> error $ "Conversao inválida : posição " ++ show (getposTokenTipo tipo)
+        TIPO _ "LOGICO" ->
+            case res1 of
+                ValorInteiro a -> (ValorLogico (intToLogico a), estado1)
+                ValorReal a -> (ValorLogico (realToLogico a), estado1)
+                ValorLogico a -> (ValorLogico a, estado1)
+                otherwise -> error $ "Conversao inválida : posição " ++ show (getposTokenTipo tipo)
+        otherwise -> error $ "Conversao inválida : posição " ++ show (getposTokenTipo tipo)
+    where
+        (res1,estado1) = evaluateExpr estado expr
+        getposTokenTipo :: Token -> (Int,Int)
+        getposTokenTipo (TIPO p _) = p
+        showLogico :: Bool -> String
+        showLogico True = "VERDADEIRO"
+        showLogico False = "FALSO"
+        logicoToInt :: Bool -> Integer
+        logicoToInt False = 0
+        logicoToInt True = 1
+        logicoToReal :: Bool -> Double
+        logicoToReal False = 0
+        logicoToReal True = 1
+        intToLogico :: Integer -> Bool
+        intToLogico 0 = False
+        intToLogico _ = True
+        realToLogico :: Double -> Bool
+        realToLogico 0 = False
+        realToLogico _ = True
+        
 evaluateExpr estado (CRIATEXTO (TEXTO _ t)) = (ValorTexto t, estado) -- OK
 evaluateExpr estado (CRIAINT (INTEIRO _ i)) = (ValorInteiro i, estado) -- OK
 evaluateExpr estado (CRIACARACTERE (CARACTERE _ c)) = (ValorCaractere c, estado) -- OK
@@ -310,6 +367,6 @@ evaluateExpr estado (CRIAPARENTESES a) = evaluateExpr estado a -- OK
     CRIACHAMADAFUNC CHAMADA |
     CRIANOVO [PONT] {-TIPO-}Token OptionalSQBRACK |
     CRIAVALOREXPR VAL |
-    CRIACONVERSAO [PONT] {-TIPO-}Token EXPR
+    CRIACONVERSAO {-TIPO-}Token EXPR
 -}
 
